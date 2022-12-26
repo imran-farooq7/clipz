b87b7a763b12c34caa840d3b54779ecc6dfa1aa0
imran farooq <imran_farooq7@hotmail.com>
2022-12-27 01:43:53 +0500

    implemented reactive form for file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,35 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+FormControl;
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+  isDragOver = false;
+  file: File | null = null;
+  nextStep = false;
+  storeFile = (event: Event) => {
+    this.isDragOver = false;
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    this.nextStep = true;
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  uploadFile = () => {
+    console.log('File Upload succeeded');
+  };
+  ngOnInit(): void {}
 }
--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -1,6 +1,7 @@
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { VideoRoutingModule } from './video-routing.module';
 import { ManageComponent } from './manage/manage.component';
@@ -8,6 +9,11 @@ import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [ManageComponent, UploadComponent],
-  imports: [CommonModule, VideoRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    VideoRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class VideoModule {}
