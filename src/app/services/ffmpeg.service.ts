dd8bd43c8a0759ff83a03881597102f76d8c2969
imran farooq <imran_farooq7@hotmail.com>
2023-01-12 01:55:25 +0500

    created screen shots url list

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -22,5 +22,34 @@ export class FFMPEGService {
   getScreenShots = async (file: File) => {
     const data = await fetchFile(file);
     this.ffmpeg.FS('writeFile', file.name, data);
+    const seconds = [1, 2, 3];
+    const commands: string[] = [];
+    seconds.forEach((second) =>
+      commands.push(
+        '-i',
+        file.name,
+        '-ss',
+        `00:00:0${second}`,
+        '-frames:v',
+        '1',
+        '-filter:v',
+        'scale=510:-1',
+        `output_1${second}.png`
+      )
+    );
+    this.ffmpeg.run(...commands);
+    const screenShots: string[] = [];
+    seconds.forEach((second) => {
+      const screenshotFile = this.ffmpeg.FS(
+        'readFile',
+        `output_1${second}.png`
+      );
+      const screenshotBlob = new Blob([screenshotFile.buffer], {
+        type: 'image/png',
+      });
+      const screenshotURL = URL.createObjectURL(screenshotBlob);
+      screenShots.push(screenshotURL);
+    });
+    return screenShots;
   };
 }
