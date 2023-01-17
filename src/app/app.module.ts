47800009195b73b5b65a29ee53e8cff7b36ea921
imran farooq <imran_farooq7@hotmail.com>
2023-01-17 23:47:43 +0500

    list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ComponentListComponent } from './component-list/component-list.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AboutComponent,
     ClipComponent,
     NotFoundComponent,
+    ComponentListComponent,
   ],
   imports: [
     BrowserModule,
